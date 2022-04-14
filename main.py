9c9aa701bbe18d13f5be13aeed1a674293432bf9
andrejdolnenec <andrej.dolnenec@gmail.com>
2022-04-14 10:52:58 +0200

    test

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import json
 
 app = Flask(__name__)
 
-data =  json.load(open('data.json'))
-#location = json.load(open('location.json'))
+#data =  json.load(open('data.json'))
+location = json.load(open('location.json'))
 
 @app.route('/get/', methods=['GET'])
 def respond():
@@ -27,7 +27,7 @@ def respond():
 
     # Return the response in json format
    # return jsonify(response)
-    return data
+    return location
 
 
 @app.route('/post/', methods=['POST'])
